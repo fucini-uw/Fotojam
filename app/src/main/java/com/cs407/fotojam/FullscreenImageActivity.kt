430d82a94c02e3973e22330d780e0cfabbb038e4
jbolson2 <jbolson2@wisc.edu>
2024-12-05 14:56:31 -0600

    fullscreenimageactivity working

--- app/src/main/java/com/cs407/fotojam/FullscreenImageActivity.kt
+++ app/src/main/java/com/cs407/fotojam/FullscreenImageActivity.kt
@@ -9,6 +9,28 @@ import com.bumptech.glide.Glide
 
 class FullscreenImageActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullscreen_image)
 
+        val fullscreenImageView: ImageView = findViewById(R.id.fullscreenImageView)
+        val exitButton: Button = findViewById(R.id.exitButton)
 
+        // Get image URL passed from the adapter
+        val imageUrl = intent.getStringExtra("imageUrl")
+        Log.d("FullscreenImageActivity", "Received URL: $imageUrl")
+
+        if (!imageUrl.isNullOrEmpty()) {
+            Glide.with(this)
+                .load(imageUrl)
+                .into(fullscreenImageView)
+        } else {
+            Log.e("FullscreenImageActivity", "No URL received.")
+        }
+
+        // Set the exit button functionality
+        exitButton.setOnClickListener {
+            finish()
+        }
+    }
 }
