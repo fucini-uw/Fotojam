fc7edd2cd66266bc01be972ae99a4d1997d679c8
jasombehoth <lgroen@wisc.edu>
2024-12-04 22:06:31 -0600

    rating screen but no working fullscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+}
+
+dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
 }
--- app/src/main/java/com/cs407/fotojam/FullscreenImageActivity.kt
+++ app/src/main/java/com/cs407/fotojam/FullscreenImageActivity.kt
@@ -1,27 +1,14 @@
 package com.cs407.fotojam
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class FullscreenImageActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fullscreen_image)
 
-        val fullscreenImageView: ImageView = findViewById(R.id.fullscreenImageView)
-        val exitButton: Button = findViewById(R.id.exitButton)
 
-        // get image resource passed from the adapter
-        val imageResource = intent.getIntExtra("imageResource", -1)
-        if (imageResource != -1) {
-            fullscreenImageView.setImageResource(imageResource)
-        }
-
-        exitButton.setOnClickListener {
-            finish()
-        }
-    }
 }
